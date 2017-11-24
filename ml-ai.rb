e363a9a1a00030cb4e68c3889c15bc3a663aff95
Meredith Lind <meredith@collectiveidea.com>
2017-11-24 13:55:11 -0500

    Select random valid column

--- ml-ai.rb
+++ ml-ai.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby -w
 # AI player for the game Connect Four written by Meredith Lind
 
-#@board = JSON.parse(ARGV[1])
-require 'json'
-@board = JSON.parse("[[1,0,0,0,0,0,0],[2,0,0,0,0,0,0],[1,0,0,0,0,0,0],[2,0,0,0,0,0,0],[2,2,0,0,0,0,0],[1,1,0,0,0,0,0]]")
+ require 'json'
+ @board = JSON.parse(ARGV[1])
+# @board = JSON.parse("[[1,0,0,0,0,0,0],[2,0,0,0,0,0,0],[1,0,0,0,0,0,0],[2,0,0,0,0,0,0],[2,2,0,0,0,0,0],[1,1,0,0,0,0,0]]")
 
 def is_valid?(y)
   get_next_opening(y) >= 0
@@ -35,12 +35,22 @@ def get_column(y)
   col
 end
 
-puts "#{get_next_opening(0)}"
-puts "#{is_valid?(0)}"
-puts "#{get_next_opening(1)}"
-puts "#{is_valid?(1)}"
-puts "#{get_next_opening(2)}"
-puts "#{get_next_opening(3)}"
-puts "#{get_next_opening(4)}"
-puts "#{get_next_opening(5)}"
-puts "#{get_next_opening(6)}"
+# puts "#{get_next_opening(0)}"
+# puts "#{is_valid?(0)}"
+# puts "#{get_next_opening(1)}"
+# puts "#{is_valid?(1)}"
+# puts "#{get_next_opening(2)}"
+# puts "#{get_next_opening(3)}"
+# puts "#{get_next_opening(4)}"
+# puts "#{get_next_opening(5)}"
+# puts "#{get_next_opening(6)}"
+
+rand = rand(0...@board[0].size)
+puts"#{rand}"
+
+until is_valid?(rand)
+  rand = rand(0...@board[0].size)
+  puts"#{rand}"
+end
+
+exit(rand)
