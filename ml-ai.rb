d80f1f6888c6549c9edb80b145a7120be80925a4
Meredith Lind <meredith@collectiveidea.com>
2017-11-24 13:07:37 -0500

    Determine if move is valid

--- ml-ai.rb
+++ ml-ai.rb
@@ -3,7 +3,23 @@
 
 #@board = JSON.parse(ARGV[1])
 require 'json'
-@board = JSON.parse("[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[2,2,0,0,0,0,0],[1,1,0,0,0,0,0]]")
+@board = JSON.parse("[[1,0,0,0,0,0,0],[2,0,0,0,0,0,0],[1,0,0,0,0,0,0],[2,0,0,0,0,0,0],[2,2,0,0,0,0,0],[1,1,0,0,0,0,0]]")
+
+def is_valid?(y)
+  get_next_opening(y) >= 0
+end
+
+def get_next_opening(y)
+  col = get_column(y)
+  # Starting at the end of the array, get the index of the first zero cell
+  # If the column is full, it returns -1
+  i = col.size - 1
+  while i >= 0 do
+    break if col[i] == 0
+    i -= 1
+  end
+  i
+end
 
 def get_column(y)
   col = []
@@ -19,9 +35,12 @@ def get_column(y)
   col
 end
 
-
-puts "#{get_column(0)}"
-puts "#{get_column(1)}"
-puts "#{get_column(2)}"
-puts "#{get_column(3)}"
-puts "#{get_column(4)}"
+puts "#{get_next_opening(0)}"
+puts "#{is_valid?(0)}"
+puts "#{get_next_opening(1)}"
+puts "#{is_valid?(1)}"
+puts "#{get_next_opening(2)}"
+puts "#{get_next_opening(3)}"
+puts "#{get_next_opening(4)}"
+puts "#{get_next_opening(5)}"
+puts "#{get_next_opening(6)}"
